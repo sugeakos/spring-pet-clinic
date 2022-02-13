3f9ebf23b51471c7291de3e0b4f8607b144b8490
sugeakos <sugeakos@gmail.com>
2022-02-13 17:45:42 +0100

    Update Map Impl to manage setting of ID property. Closes #22

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetServiceMap.java
@@ -21,7 +21,7 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
 
     @Override
     public Pet save(Pet object) {
-        return save(object.getId(),object);
+        return save(object);
     }
 
     @Override
