123f1249c0c906c7d99ba0f394737e7d247da912
sugeakos <sugeakos@gmail.com>
2022-02-19 12:48:11 +0100

    implemented repositories. Closes #38

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.petclinic.repositories;
+
+import guru.springframework.petclinic.model.Specialty;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
+
+}
