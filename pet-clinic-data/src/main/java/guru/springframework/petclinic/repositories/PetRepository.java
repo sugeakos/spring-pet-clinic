89d9d29687c30476bd29dd77aca9d5ad8df3d9f3
sugeakos <sugeakos@gmail.com>
2022-02-19 13:04:26 +0100

    Spring Data JPA for Owners. Closes #39

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/repositories/PetRepository.java
@@ -3,5 +3,5 @@ package guru.springframework.petclinic.repositories;
 import guru.springframework.petclinic.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PerRepository extends CrudRepository<Pet, Long> {
+public interface PetRepository extends CrudRepository<Pet, Long> {
 }
