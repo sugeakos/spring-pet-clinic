277d971e84b62e7b3026687a6ec09618a4ec8631
sugeakos <sugeakos@gmail.com>
2022-02-20 21:16:03 +0100

    refactor the project using Project Lombok. Closes #50

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Visit.java
@@ -1,10 +1,16 @@
 package guru.springframework.petclinic.model;
 
+import lombok.*;
 import org.hibernate.annotations.CollectionId;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity{
@@ -19,27 +25,7 @@ public class Visit extends BaseEntity{
     @JoinColumn(name = "pet_id")
     private Pet pet;
 
-    public LocalDate getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
 
-    public String getDescription() {
-        return description;
-    }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
